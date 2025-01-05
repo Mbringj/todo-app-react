dee16f39550707d9ef0d715db405466febb5af98
Mbring Jeremie <mbringjeremy@gmail.com>
2025-01-05 22:56:40 +0100

    add todo add, edit, delete and track for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
-interface User {
-  name: string;
-  id: number;
-}
+// interface User {
+//   name: string;
+//   id: number;
+// }
 
 // type stringArray = Array<string>;
 // type NumberArray = Array<number>;
 // type ObjectWidthNameArray = Array<{name: string}>
 
 
-const user: User = {
-  name: "mbring",
-  id: 34
-}
+// const user: User = {
+//   name: "mbring",
+//   id: 34
+// }
 
-class UserAcount {
-  name: string;
-  id: number;
+// class UserAcount {
+//   name: string;
+//   id: number;
 
-  constructor(name:string, id:number) {
-    this.name = name;
-    this.id = id;
-  }
-}
+//   constructor(name:string, id:number) {
+//     this.name = name;
+//     this.id = id;
+//   }
+// }
 
-const users: UserAcount = new UserAcount('Mbring', 89);
+// const users: UserAcount = new UserAcount('Mbring', 89);
 
 function App() {
 
   return (
     <>
-      <h1>utilisateur: <br />
-        Name: {user.name}
-        Id: {user.id}
-      </h1>
-      <h1> users { users.name}</h1>
+      <h1>Hello world</h1>
     </>
   )
 }
